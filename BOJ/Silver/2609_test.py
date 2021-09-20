73efffe53f4eda662b65a5c68211daf8d18cdf2a
rachel5004 <j26597719@naver.com>
2021-09-20 16:55:25 +0900

    (pytest) BOJ 2609

--- BOJ/Silver/2609_test.py
+++ BOJ/Silver/2609_test.py
@@ -4,14 +4,21 @@ def solution(n,m):
     gcd=lcm=1
     n,m=min(n,m),max(n,m)
     if m%n==0: return [n,m]
-    for i in range(2, int(math.sqrt(m)) + 1):
-        while True:
-            if n % i == 0 and m % i == 0:
-                gcd *= i
-                n, m = n // i, m // i
-            else: break
+    for i in range(2, n + 1):
+        if is_prime_number(i):
+            while True:
+                if n % i == 0 and m % i == 0:
+                    gcd *= i
+                    n, m = n // i, m // i
+                else: break
     lcm=gcd*n*m
     return [gcd,lcm]
+
+def is_prime_number(x):
+    for i in range(2, int(math.sqrt(x))+1):
+        if x % i == 0:
+            return False
+    return True
 
 def Euclidean_gcd(n,m):
     while m: n,m=m,n%m
@@ -27,5 +34,5 @@ def test_case():
     assert Euclidean_gcd(24,18)==6
     assert Euclidean_lcm(24,18) == 72
     # with solution
-    n,m=373,742
+    n,m=10,15
     assert solution(n,m)==[math.gcd(n, m),math.lcm(n,m)]
