e71db489cd456c88766d63feec0a33611ae22ebe
rachel3486 <rachel3486@gmail.com>
2023-04-20 22:06:19 +0900

    [PRM] 괄호 회전하기 - 13 case solve

--- Kakao/bracket.py
+++ Kakao/bracket.py
@@ -4,14 +4,13 @@ def solution(s):
         "(":")",
         "[":"]"
             }
-
     cnt=0
     for n in range(len(s)):
-        tmp = s if n==0 else s[n:]+s[:n-1]
+        s = s if n==0 else s[1:]+s[0]
         stack = []
         flag=True
-        for i in list(tmp):
+        for i in list(s):
             if i in start: stack.append(i)
             elif not stack or i!=start[stack.pop()]: flag=False;continue;
-        if flag: cnt+=1
+        if not stack and flag: cnt+=1
     return cnt
