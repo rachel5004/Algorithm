0280817dd56b0e092ebc40317e99bd0d81cfdd87
rachel3486 <rachel3486@gmail.com>
2023-04-24 23:33:37 +0900

    [PRM] 순위검색

--- Kakao/bracket.py
+++ Kakao/bracket.py
@@ -14,3 +14,36 @@ def solution(s):
             elif not stack or i!=start[stack.pop()]: flag=False;continue;
         if not stack and flag: cnt+=1
     return cnt
+
+# solved
+def solution(info, query):
+    data = dict()
+    for a in ['cpp', 'java', 'python', '-']:
+        for b in ['backend', 'frontend', '-']:
+            for c in ['junior', 'senior', '-']:
+                for d in ['chicken', 'pizza', '-']:
+                    data.setdefault((a, b, c, d), list())
+    for i in info:
+        i = i.split()
+        for a in [i[0], '-']:
+            for b in [i[1], '-']:
+                for c in [i[2], '-']:
+                    for d in [i[3], '-']:
+                        data[(a, b, c, d)].append(int(i[4]))
+
+    for k in data:
+        data[k].sort()
+
+    answer = []
+    for q in query:
+        q = q.replace('and ', '').split()
+
+        pool = data[(q[0], q[1], q[2], q[3])]
+        find = int(q[4])
+        l,r,mid = 0,len(pool),0
+        while l < r:
+            mid = (r+l)//2
+            if pool[mid] >= find:r = mid
+            else: l = mid+1
+        answer.append(len(pool)-l)
+    return answer
